f2afbcb521d7b43a8e4c76f2343ccc04751c39b8
Tuomas Jormola <tj@suula.io>
2018-02-28 17:05:31 +0200

    Implement decoding of Avro generated Java classes
    This feature introduces dependency on gradle-avro-plugin
    in order to compile the unit test support class.

--- converter/src/main/java/tech/allegro/schema/json2avro/converter/JsonAvroConverter.java
+++ converter/src/main/java/tech/allegro/schema/json2avro/converter/JsonAvroConverter.java
@@ -7,11 +7,16 @@ import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.BinaryDecoder;
 import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.Decoder;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.io.NoWrappingJsonEncoder;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.avro.specific.SpecificRecordBase;
 import org.codehaus.jackson.map.ObjectMapper;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
@@ -45,6 +50,23 @@ public class JsonAvroConverter {
 
     public GenericData.Record convertToGenericDataRecord(byte[] data, Schema schema) {
         return recordReader.read(data, schema);
+    }
+
+    public <T extends SpecificRecordBase & SpecificRecord> T convertToSpecificRecord(byte[] jsonData, Class<T> clazz, Schema schema) {
+        byte[] avroBinaryData = this.convertToAvro(jsonData, schema);
+        SpecificDatumReader<T> reader = new SpecificDatumReader<T>(clazz);
+        ByteArrayInputStream inStream = new ByteArrayInputStream(avroBinaryData);
+        Decoder binaryDecoder = DecoderFactory.get().directBinaryDecoder(inStream, null);
+        try {
+            Decoder decoder = DecoderFactory.get().validatingDecoder(schema, binaryDecoder);
+            return reader.read(null, decoder);
+        } catch (IOException e) {
+            throw new AvroConversionException("Failed to convert to AVRO.", e);
+        }
+    }
+
+    public <T extends SpecificRecordBase & SpecificRecord> T convertToSpecificRecord(byte[] data, Class<T> clazz, String schema) {
+        return convertToSpecificRecord(data, clazz, new Schema.Parser().parse(schema));
     }
 
     public byte[] convertToJson(byte[] avro, String schema) {
