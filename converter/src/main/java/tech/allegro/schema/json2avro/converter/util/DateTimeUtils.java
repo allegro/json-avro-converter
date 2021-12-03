60c174a228e3dd9647d49c2dca78a7b526d6c7fa
Liren Tu <tuliren.git@outlook.com>
2021-12-02 17:51:56 -0800

    Add comments

--- converter/src/main/java/tech/allegro/schema/json2avro/converter/util/DateTimeUtils.java
+++ converter/src/main/java/tech/allegro/schema/json2avro/converter/util/DateTimeUtils.java
@@ -17,18 +17,21 @@ public class DateTimeUtils {
     private static final DateTimeFormatter timeFormatter =
             DateTimeFormatter.ofPattern("HH:mm[':'ss[.][SSSSSS][SSSSS][SSSS][SSS]]");
 
-    // return the number of microseconds from the unix epoch, 1 January 1970 00:00:00.000000 UTC.
-    public static Long getEpochMicros(String dateTime) {
+    /**
+     * Parse the Json date-time logical type to an Avro long value.
+     * @return the number of microseconds from the unix epoch, 1 January 1970 00:00:00.000000 UTC.
+     */
+    public static Long getEpochMicros(String jsonDateTime) {
         Instant instant = null;
-        if (dateTime.matches("-?\\d+")) {
-            return Long.valueOf(dateTime);
+        if (jsonDateTime.matches("-?\\d+")) {
+            return Long.valueOf(jsonDateTime);
         }
         try {
-            ZonedDateTime zdt = ZonedDateTime.parse(dateTime, formatter);
+            ZonedDateTime zdt = ZonedDateTime.parse(jsonDateTime, formatter);
             instant = zdt.toInstant();
         } catch (DateTimeParseException e) {
             try {
-                LocalDateTime dt = LocalDateTime.parse(dateTime, formatter);
+                LocalDateTime dt = LocalDateTime.parse(jsonDateTime, formatter);
                 instant = dt.toInstant(ZoneOffset.UTC);
             } catch (DateTimeParseException ex) {
                 // no logging since it may generate too much noise
@@ -37,11 +40,14 @@ public class DateTimeUtils {
         return instant == null ? null : instant.toEpochMilli() * 1000;
     }
 
-    // returns the number of days from the unix epoch, 1 January 1970 (ISO calendar).
-    public static Integer getEpochDay(String dateTime) {
+    /**
+     * Parse the Json date logical type to an Avro int.
+     * @return the number of days from the unix epoch, 1 January 1970 (ISO calendar).
+     */
+    public static Integer getEpochDay(String jsonDate) {
         Integer epochDay = null;
         try {
-            LocalDate date = LocalDate.parse(dateTime, formatter);
+            LocalDate date = LocalDate.parse(jsonDate, formatter);
             epochDay = (int) date.toEpochDay();
         } catch (DateTimeParseException e) {
             // no logging since it may generate too much noise
@@ -49,18 +55,21 @@ public class DateTimeUtils {
         return epochDay;
     }
 
-    // returns the number of microseconds after midnight, 00:00:00.000000.
-    public static Long getMicroSeconds(String dateTime) {
+    /**
+     * Parse the Json time logical type to an Avro long.
+     * @return the number of microseconds after midnight, 00:00:00.000000.
+     */
+    public static Long getMicroSeconds(String jsonTime) {
         Long nanoOfDay = null;
-        if (dateTime.matches("-?\\d+")) {
-            return Long.valueOf(dateTime);
+        if (jsonTime.matches("-?\\d+")) {
+            return Long.valueOf(jsonTime);
         }
         try {
-            LocalTime time = LocalTime.parse(dateTime, timeFormatter);
+            LocalTime time = LocalTime.parse(jsonTime, timeFormatter);
             nanoOfDay = time.toNanoOfDay();
         } catch (DateTimeParseException e) {
             try {
-                LocalTime time = LocalTime.parse(dateTime, formatter);
+                LocalTime time = LocalTime.parse(jsonTime, formatter);
                 nanoOfDay = time.toNanoOfDay();
             } catch (DateTimeParseException ex) {
                 // no logging since it may generate too much noise
