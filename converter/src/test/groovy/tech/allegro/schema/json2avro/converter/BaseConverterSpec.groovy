e23fc8bf5454ad99b5da97783409ade77c431fa1
mateusz.dobrowolski <mateusz.dobrowolski@allegro.com>
2025-03-10 09:40:51 +0100

    allegro/hermes#1808 | Decimals in JSON as String
    allegro/hermes#1808 | java 17
    
    allegro/hermes#1808 | cache

--- converter/src/test/groovy/tech/allegro/schema/json2avro/converter/BaseConverterSpec.groovy
+++ converter/src/test/groovy/tech/allegro/schema/json2avro/converter/BaseConverterSpec.groovy
@@ -6,9 +6,17 @@ import spock.lang.Specification
 
 class BaseConverterSpec extends Specification {
 
-    def converter = new JsonAvroConverter(new ObjectMapper(),
+    def avroConverter = new JsonAvroConverter(new ObjectMapper(),
             {name, value, path -> println "Unknown field $path with value $value"})
+    def jsonConverter = new AvroJsonConverter()
     def converterFailOnUnknown = new JsonAvroConverter(new ObjectMapper(), new FailOnUnknownField())
     def slurper = new JsonSlurper()
 
+    def toMap(byte[] json) {
+        slurper.parse(json)
+    }
+
+    def toMap(String json) {
+        slurper.parseText(json)
+    }
 }
